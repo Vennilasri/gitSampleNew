ad90d8c4f58e27cc01031987168810e0b9f83d24
vennilasri321 <vennilasri321@gmail.com>
2023-12-02 15:05:29 +0530

    new initialization

---
+++ src/org/pack/GitClass.java
@@ -0,0 +1,22 @@
+package org.pack;
+
+public class GitClass {
+	
+	public void tc1() {
+		
+		System.out.println("tamilNadu");
+	}
+	
+	public void tc2() {
+		System.out.println("kerala");
+	
+
+	}
+	public void tc3() {
+		System.out.println("karnataka");
+	
+
+	}
+
+
+}
